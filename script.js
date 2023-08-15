c9696b0d0b4b2cedbf107a60299fa5b827004b14
JOYSTON <116254639+Joystonm@users.noreply.github.com>
2023-08-15 11:23:30 +0530

    q

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Heading animation
 var typed= new Typed(".text",{
     strings:["Front End Developer","UI/UX Designer"],
     typeSpeed:100,
@@ -5,9 +6,13 @@ var typed= new Typed(".text",{
     loop:true
 })
 
+// Progress bar animation
 const filled=document.querySelector(".progress-bar")
 function update(){
     filled.style.width=`${((window.scrollY)/(document.body.scrollHeight-window.innerHeight)*100)}%`
     requestAnimationFrame(update);
 }
 update()
+
+
+
